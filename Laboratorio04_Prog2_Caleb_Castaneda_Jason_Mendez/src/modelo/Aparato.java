ad7331df17942a18577fe3b5a92c669a0104fb95
Caleb UCR <Caleb UCR@DESKTOP-KIOVDD8>
2021-05-08 20:14:19 -0600

    creando clases en modelo

---
+++ Laboratorio04_Prog2_Caleb_Castaneda_Jason_Mendez/src/modelo/Aparato.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Aparato {
+	
+	private String marca;
+	
+	private String serie;
+	
+	private String modelo;
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getSerie() {
+		return serie;
+	}
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+
+}
---
+++ Laboratorio04_Prog2_Caleb_Castaneda_Jason_Mendez/src/modelo/Persona.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Persona {
+	
+	private String nombre;
+	
+	private String id;
+	
+	private String numTel;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNumTel() {
+		return numTel;
+	}
+
+	public void setNumTel(String numTel) {
+		this.numTel = numTel;
+	}
+	
+	
+
+}
