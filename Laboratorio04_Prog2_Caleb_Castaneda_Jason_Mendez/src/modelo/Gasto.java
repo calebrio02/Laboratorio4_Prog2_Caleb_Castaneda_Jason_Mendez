10c6de348c87147aecc3d7b6c804e699aba7975a
Caleb UCR <Caleb UCR@DESKTOP-KIOVDD8>
2021-05-09 10:09:18 -0600

    10:09 am

---
+++ Laboratorio04_Prog2_Caleb_Castaneda_Jason_Mendez/src/modelo/Gasto.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class Gasto {
+	
+	private int ingreso;
+	
+	private int gastoTotal;
+
+	public int getIngreso() {
+		return ingreso;
+	}
+
+	public void setIngreso(int ingreso) {
+		this.ingreso = ingreso;
+	}
+
+	public int getGastoTotal() {
+		return gastoTotal;
+	}
+
+	public void setGastoTotal(int gastoTotal) {
+		this.gastoTotal = gastoTotal;
+	}
+	
+	
+
+}
