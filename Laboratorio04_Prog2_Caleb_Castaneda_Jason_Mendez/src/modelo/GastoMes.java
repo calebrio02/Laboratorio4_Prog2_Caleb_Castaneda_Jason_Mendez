4ed2ba7eb71059db22060b5885bc56bbeccb9fdb
Caleb UCR <Caleb UCR@DESKTOP-KIOVDD8>
2021-05-09 11:02:03 -0600

    11:01

---
+++ Laboratorio04_Prog2_Caleb_Castaneda_Jason_Mendez/src/modelo/GastoMes.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class GastoMes {
+	
+	GServiciosBasicos sbasico = new GServiciosBasicos();
+	
+	GComida comida = new GComida();
+	
+	GOcio ocio = new GOcio();
+
+	public GastoMes() {
+		
+		
+	}
+	
+	
+
+}
